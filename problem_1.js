77c07a8b44b593f58edf14d7b4fadc3beea8efe4
Mikita <nikita.492@yandex.by>
2023-04-22 02:37:19 +0300

    Refactor problem_1

--- problem_1.js
+++ problem_1.js
@@ -1,5 +1,24 @@
-let sum = 0;
-for (let i = 0; i < 1000; i++)
-	if (i % 3 === 0 || i % 5 == 0)
-		sum += i;
-console.log(sum);
+const getLastDivider = divider => {
+	const max = 1000;
+	for (let i = max - 1; i > 0; i--) {
+		if (i % divider === 0) {
+			return i;
+		}
+	}
+	return 0;
+}
+
+const getCountOfNum = (lastDiv, div) => ((lastDiv - div) / div + 1) | 0;
+
+const getSumByDiv = (div, lastDiv, count) => ((div + lastDiv) / 2) * count;
+
+const getSumOfMul = () => {
+	const lastDivThree = getLastDivider(3);
+	const lastDivFive = getLastDivider(5);
+	const lastDivFifteen = getLastDivider(15);
+	return getSumByDiv(3, lastDivThree, getCountOfNum(lastDivThree, 3)) +
+		getSumByDiv(5, lastDivFive, getCountOfNum(lastDivThree, 5)) -
+		getSumByDiv(15, lastDivFifteen, getCountOfNum(lastDivFifteen, 15));
+}
+
+console.log(getSumOfMul());
