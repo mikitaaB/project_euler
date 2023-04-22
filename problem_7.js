1446cdae23d28cc817e089fcfd8a85afe87208c0
Mikita <nikita.492@yandex.by>
2023-04-22 17:10:51 +0300

    Refactor problem_7

--- problem_7.js
+++ problem_7.js
@@ -1,19 +1,23 @@
-function isPrime(num) {
-	for (var i = 3; Math.pow(i, 2) <= num; i += 2)
-		if (num % i == 0)
+const isPrime = num => {
+	for (let i = 3; i ** 2 <= num; i += 2) {
+		if (num % i == 0) {
 			return false;
+		}
+	}
 	return true;
 }
 
-function checkPrimeByIndex(newIndex){
-	var index = 1;
-	var num = 1;
-	if (newIndex == 1)
+const checkPrimeByIndex = newIndex => {
+	let index = 1;
+	let num = 1;
+	if (newIndex === 1) {
 		return 2;
+	}
 	while (index < newIndex) {
 		num += 2;
-		if (isPrime(num))
+		if (isPrime(num)) {
 			index++;
+		}
 	}
 	return num;
 }
