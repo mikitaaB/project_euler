1e9e913aabaa063ac82fa74f5b355522e7f0a3cc
Mikita <nikita.492@yandex.by>
2023-04-22 14:39:53 +0300

    Refactor problem_5

--- problem_5.js
+++ problem_5.js
@@ -1,14 +1,16 @@
+const checkIsAllDivided = num => {
+	for (let i = 11; i <= 20; i++) {
+		if (num % i !== 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
 const getSmallestMultiple = () => {
 	for (let num = 2520; ; num += 2520) {
-		for (let i = 11; i <= 20; i++) {
-			if (num % i === 0) {
-				if (i === 20) {
-					return num;
-				}
-				continue;
-			} else {
-				break;
-			}
+		if (checkIsAllDivided(num)) {
+			return num;
 		}
 	}
 }
