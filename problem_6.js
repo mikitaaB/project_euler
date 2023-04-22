53572d5d1ac9c3627fecd89e82fbb7fee7b16f57
Mikita <nikita.492@yandex.by>
2023-04-22 05:31:08 +0300

    Refactor problem_6

--- problem_6.js
+++ problem_6.js
@@ -1,6 +1,10 @@
-var sum1 = 0, sum2 = 0;
-for (var i = 1; i <= 100; i++){
-	sum1 += i;
-	sum2 += Math.pow(i,2);
+const getSquareOfSum = n => (((1 + n) * n) / 2) ** 2;
+
+const getSumOfSquares = n => {
+	if (n === 0) {
+		return 0;
+	}
+	return n ** 2 + getSumOfSquares(n - 1);
 }
-console.log(Math.pow(sum1, 2) - sum2);
+
+console.log(getSquareOfSum(100) - getSumOfSquares(100));
