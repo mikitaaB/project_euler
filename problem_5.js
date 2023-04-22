75241e743a437864e6524bf08f14147c35121c92
Mikita <nikita.492@yandex.by>
2023-04-22 04:42:42 +0300

    Refactor problem_5

--- problem_5.js
+++ problem_5.js
@@ -1,19 +1,16 @@
-var divided = false;
-var k = 0;
-while (!divided){
-	for (var num = 2520; ; num += 2520){
-		for (var i = 11; i <= 20; i++){
-			if (num % i == 0)
-				k++;
+const getSmallestMultiple = () => {
+	for (let num = 2520; ; num += 2520) {
+		for (let i = 11; i <= 20; i++) {
+			if (num % i === 0) {
+				if (i === 20) {
+					return num;
+				}
+				continue;
+			} else {
+				break;
+			}
 		}
-		if (k == 10) {
-			console.log(num);
-			divided = true;
-			break;
-		}
-		else
-			k = 0;
-		if (divided == true)
-			break;
 	}
 }
+
+console.log(getSmallestMultiple());
