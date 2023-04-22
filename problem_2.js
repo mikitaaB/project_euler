2a7a62da10d9c0a3e298b8a59fc3ec38a5c4ed1d
Mikita <nikita.492@yandex.by>
2023-04-22 03:18:21 +0300

    Refactor problem_2

--- problem_2.js
+++ problem_2.js
@@ -1,10 +1,13 @@
-let sum = 0;
-let a = 1, b = 2, c = 0;
-while (a < 4000000) {
-	if (a % 2 === 0)
-	sum += a;
-	c = a + b;
-	a = b;
-	b = c;
+const getEvenFibonacci = () => {
+	let sum = 0;
+	let a = 1, b = 2;
+	while (a < 4000000) {
+		if (a % 2 === 0) {
+			sum += a;
+		}
+		[a, b] = [b, a + b];
+	}
+	return sum;
 }
-console.log(sum);
+
+console.log(getEvenFibonacci());
