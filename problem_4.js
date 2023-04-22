08f8a45e0aa9a6c62e270a73a5dd7a5962844e35
Mikita <nikita.492@yandex.by>
2023-04-22 04:18:40 +0300

    Refactor problem_4

--- problem_4.js
+++ problem_4.js
@@ -1,19 +1,38 @@
-var num;
-var palindrome = [];
-
-for (var i = 100; i <= 999; i++){
-	for (var j = 100; j <= 999; j++){
-	num = i * j;
-	if (String(num).split('').reverse().join('') == num){
-		palindrome.push(num);
+const isPalindrome = mul => {
+	let revertMul = 0;
+	let copyMul = mul;
+	while (copyMul) {
+		revertMul = (copyMul % 10) + (revertMul * 10);
+		copyMul = (copyMul / 10) | 0;
 	}
-	}
+	return mul === revertMul;
 }
 
-var max = palindrome[0];
-for (i = 0; i < palindrome.length; i++){
-	if (palindrome[i] > max)
-	max = palindrome[i];
+const getMaxElOfArray = (arr) => {
+	let max = arr[0];
+	const arrLength = arr.length;
+	for (let i = 0; i < arrLength; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	return max;
 }
 
-console.log(max)
+const getLargestPalindromeProduct = () => {
+	let mul = 0;
+	const palindromes = [];
+
+	for (let i = 999; i >= 100; i--) {
+		for (let j = 999; j >= 100; j--) {
+			mul = i * j;
+			if (isPalindrome(mul)) {
+				palindromes.push(mul);
+			}
+		}
+	}
+
+	return getMaxElOfArray(palindromes);
+}
+
+console.log(getLargestPalindromeProduct());
