0a4a4d092b54001b5f3a59c5e9a55f2da59b1240
Mikita <nikita.492@yandex.by>
2023-05-16 01:22:22 +0300

    Refactor problem 14

--- problem_14.js
+++ problem_14.js
@@ -1,22 +1,36 @@
-function LongestCollatzSequence (limit) {
-	var startNum = 0;
-	var maxLen = 0;
-	for (var i = 2; i <= limit; i++) {
-		var len = 1;
+console.time('FirstWay');
+
+const getLongestCollatzSequence = limit => {
+	let longestSequence = 0;
+	const lowestLimit = limit/2;
+	let maxLen = 0;
+	const cache = {};
+	for (let i = lowestLimit; i < limit; i++) {
+		let len = 1;
 		num = i;
 		while (num != 1) {
-			if (num % 2 == 0)
+			if (cache[num]) {
+				len += cache[num] - 1;
+				break;
+			}
+			if (num % 2 === 0) {
 				num /= 2;
-			else
+			} else {
 				num = num * 3 + 1;
+			}
 			len++;
 		}
+		cache[i] = len;
+
 		if (len > maxLen){
 			maxLen = len;
-			startNum = i;
+			longestSequence = i;
 		}
 	}
-	return startNum;
+	return longestSequence;
 }
 
-console.log(LongestCollatzSequence(1000000));
+console.log(getLongestCollatzSequence(1000000));
+
+
+console.timeEnd('FirstWay');
