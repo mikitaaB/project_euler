f5579b61fc65f7a65df99705aca2750b0c29d2c6
Mikita <nikita.492@yandex.by>
2023-05-15 16:28:26 +0300

    Refactor problem 36

--- problem_36.js
+++ problem_36.js
@@ -1,12 +1,22 @@
-function isPalindrome(num, base){
-	num = num.toString(base);
-	if (String(num).split('').reverse().join('') == num)
-		return true;
-}
-var num = 1, sum = 0;
-while (num < 1000000) {
-	if (isPalindrome(num, 10) && isPalindrome(num, 2))
-		sum += num;
-	num++;
-}
-console.log(sum);
+const isPalindrome = (num) => {
+	let revertNum = "";
+	const numLength = num.length;
+	for (let i = numLength - 1; i >= 0; i--) {
+		revertNum += num[i];
+	}
+	return num === revertNum;
+};
+
+const getDoubleBasePalindromes = (limit) => {
+	let num = 1;
+	let sum = 0;
+	while (num < limit) {
+		if (isPalindrome(num + "") && isPalindrome(num.toString(2))) {
+			sum += num;
+		}
+		num++;
+	}
+	return sum;
+};
+
+console.log(getDoubleBasePalindromes(1000000));
